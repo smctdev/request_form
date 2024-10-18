880855f495fc1847a147fe5264f29b55984c5910
emi-Meg <smeg3478@gmail.com>
2024-10-18 16:11:21 +0800

    Update UserController.php

--- server/app/Http/Controllers/Api/UserController.php
+++ server/app/Http/Controllers/Api/UserController.php
@@ -412,7 +412,7 @@ class UserController extends Controller
 
         if ($request->hasFile('profile_picture')) {
             $file = $request->file('profile_picture');
-            $path = $file->store('request_form_profile_pictures, 'd_drive');
+            $path = $file->store('request_form_profile_pictures', 'd_drive');
             $user->profile_picture = $path;
         }
 
@@ -441,7 +441,7 @@ class UserController extends Controller
             }
 
             $file = $request->file('profile_picture');
-            $path = $file->store('request_form_profile_pictures, 'd_drive');
+            $path = $file->store('request_form_profile_pictures', 'd_drive');
             $user->profile_picture = $path;
         }
 
