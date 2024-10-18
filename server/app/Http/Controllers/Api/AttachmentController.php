aaec8017e55aadf7a888042f6f219dc99b7f58e5
Allan Justine Mascariñas <90177495+allanjustine@users.noreply.github.com>
2024-10-18 11:21:18 +0800

    Move the file path into trunas file #82
    Move the file path into trunas file #82

--- server/app/Http/Controllers/Api/AttachmentController.php
+++ server/app/Http/Controllers/Api/AttachmentController.php
@@ -6,6 +6,8 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\Attachment;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\File;
+use Illuminate\Support\Facades\Storage;
 
 class AttachmentController extends Controller
 {
@@ -15,17 +17,17 @@ class AttachmentController extends Controller
         $request->validate([
             'attachment' => 'required|mimes:pdf,jpg,jpeg,png|max:2048',
         ]);
-    
+
         try {
             $attachmentFile = $request->file('attachment');
-            $path = $attachmentFile->store('attachments', 'public');
+            $path = $attachmentFile->store('request_form_attachments', 'd_drive');
             $filename = $attachmentFile->getClientOriginalName();
-    
+
             $createdAttachment = Attachment::create([
                 'file_name' => $filename,  // Include file_name here
-                'file_path' => $path, 
+                'file_path' => $path,
             ]);
-    
+
             return response()->json([
                 'message' => 'Attachment uploaded successfully',
                 'attachment_id' => $createdAttachment->id,
@@ -38,5 +40,20 @@ class AttachmentController extends Controller
             ], 500);
         }
     }
-    
+
+    public function getFile($filePath)
+    {
+        // Assuming $filePath is something like 'rattachment/l1oNfvqICCiwpxao9a8YbfaJxPlgMjpC0EPdWHJZ.png'
+        $fullPath = Storage::disk('d_drive')->path($filePath);
+
+        if (!File::exists($fullPath)) {
+            abort(404, 'File not found');
+        }
+
+        // Get the file content and mime type
+        $file = File::get($fullPath);
+        $type = File::mimeType($fullPath);
+
+        return response($file, 200)->header("Content-Type", $type);
+    }
 }
--- server/routes/web.php
+++ server/routes/web.php
@@ -1,7 +1,12 @@
 <?php
 
+use App\Http\Controllers\API\AttachmentController;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Str;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/request-form-files/{filePath}', [AttachmentController::class, 'getFile'])->where('filePath', '.*');
+
